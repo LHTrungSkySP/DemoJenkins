5764985f2bcfa2048a1800b1ffb892b3dc37ecca
Trung Lê Hoàng <trunglh@pmr.vn>
2025-08-07 15:09:12 +0700

    434

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -32,7 +32,7 @@ namespace DemoJenkins.Controllers
         [HttpGet("Test")]
         public string Test()
         {
-            return "Test endpoint is wofgxrking!";
+            return "Test endpoint is wofgxrkinjioj786786g!";
         }
     }
 }
